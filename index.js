e4e72d51e76aed6459b03149bcb237058033b61c
Glitch (lowly-verbose-beard) <none>
2024-11-04 14:55:38 +0000

    ✅📭 Updated with Glitch

--- index.js
+++ index.js
@@ -2,31 +2,54 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
-
-
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
+});
+
+let responseObj = {};
+app.get("/api/:input", (request, response) => {
+  let input = request.params.input;
+  let date = new Date(input);
+
+  if (!isNaN(date)) {
+    responseObj["utc"] = date.toUTCString();
+    responseObj["unix"] = date.getTime();
+  } else {
+    let converted = parseInt(input);
+    responseObj["unix"] = new Date(converted).getTime();
+    responseObj["utc"] = new Date(converted).toUTCString();
+  }
+
+  if (!responseObj["unix"] || !responseObj["utc"]) {
+    responseObj = { error: "Invalid Date" };
+  }
+  response.json(responseObj);
+});
+
+app.get("/api", (request, response) => {
+  responseObj["utc"] = new Date().toUTCString();
+  responseObj["unix"] = new Date().getTime();
+  response.json(responseObj);
 });
